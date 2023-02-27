b6c33bcae4df65d4d686cd7516c0fc763616167a
Anatoliy <zaghalskiy@mail.ru>
2023-02-28 01:58:50 +0300

    Wrote a program

---
+++ Programm/StrLengthArr/StrLengthArr/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter the number of rows in the array");
+int number = Convert.ToInt32(Console.ReadLine());
+StrArr strArr = new();
+string[] strArr1 = strArr.FillArray(number);
+strArr.PrintArray(strArr1);
+Console.Write("->");
+strArr.PrintArray(strArr.LengthStrArr(3,strArr1));
---
+++ Programm/StrLengthArr/StrLengthArr/StrArr.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class StrArr
+    {
+    public string[] FillArray(int number)
+    {
+        string[] strArr= new string[number];
+        for (int i = 0; i < number; i++)
+        {
+            strArr[i] = Console.ReadLine();
+        }
+        return strArr;
+    }
+    public string[] LengthStrArr(int number, string[] array)
+    {
+        string[] strArrLength = new string[0];
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= number)
+            {
+     
+                    Array.Resize(ref strArrLength, count + 1);
+                    strArrLength[count] = array[i];
+                    count++;
+                
+            }
+        }
+        return strArrLength;
+    }
+    public void PrintArray(string[] array)
+    {
+        Console.Write("[");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write("\""+array[i]+"\",");
+            }
+
+        Console.Write("\b]");
+    }
+}
